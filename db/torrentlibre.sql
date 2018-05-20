a5e48ff5fab79ff2ab2c7bcef1d28149efda334c
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-05-21 00:16:53 +0200

    Mejoro formateado

--- db/torrentlibre.sql
+++ db/torrentlibre.sql
@@ -3,98 +3,86 @@
 ------------------------------
 
 ---------------------------------------------------
--- Roles --
+--                     Roles                     --
 ---------------------------------------------------
-
 DROP TABLE IF EXISTS roles CASCADE;
-
-CREATE TABLE roles
-(
-    id         BIGSERIAL    PRIMARY KEY
-  , tipo     varchar(255) NOT NULL UNIQUE
+CREATE TABLE roles (
+    id               BIGSERIAL     PRIMARY KEY
+  , tipo             VARCHAR(255)  NOT NULL UNIQUE
 );
 
----------------------------------------------------
--- Usuarios--
----------------------------------------------------
+CREATE INDEX idx_roles_tipo ON roles (tipo);
 
+---------------------------------------------------
+--                    Usuarios                   --
+---------------------------------------------------
 DROP TABLE IF EXISTS usuarios CASCADE;
-
-CREATE TABLE usuarios
-(
-    id         BIGSERIAL    PRIMARY KEY
-  , password   varchar(255) NOT NULL
-  , auth_key   varchar(255)
-  , token      varchar(255) UNIQUE
-  , created_at TIMESTAMP(0) DEFAULT localtimestamp
-  , updated_at TIMESTAMP(0) DEFAULT localtimestamp
-  , rol_id        BIGINT       DEFAULT 1
-                            NOT NULL REFERENCES roles (id)
-                            ON DELETE NO ACTION
-                            ON UPDATE CASCADE
+CREATE TABLE usuarios (
+    id               BIGSERIAL     PRIMARY KEY
+  , password         VARCHAR(255)  NOT NULL
+  , email            VARCHAR(255)  NOT NULL UNIQUE
+  , auth_key         VARCHAR(255)
+  , token            VARCHAR(255)  UNIQUE
+  , created_at       TIMESTAMP(0)  DEFAULT LOCALTIMESTAMP
+  , updated_at       TIMESTAMP(0)  DEFAULT LOCALTIMESTAMP
+  , datos_id         BIGINT        REFERENCES usuarios_datos(id)
+  , rol_id           BIGINT        DEFAULT 1
+                                   NOT NULL REFERENCES roles (id)
+                                   ON DELETE NO ACTION
+                                   ON UPDATE CASCADE
 );
 
 CREATE INDEX idx_usuarios_email ON usuarios (email);
 
 
-
 DROP TABLE IF EXISTS usuarios_datos CASCADE;
-
-CREATE TABLE usuarios_datos
-(
-    id_usuario       bigint       PRIMARY KEY REFERENCES usuarios (id)
-    ON DELETE CASCADE
-  , nombre           varchar(255)
-  , nick             varchar(255) NOT NULL UNIQUE
-  , email            varchar(255) NOT NULL UNIQUE
-  , localidad        varchar(255)
-  , provincia        varchar(255)
-  , direccion        varchar(255)
-  , telefono         varchar(255)
-  , biografia        varchar(255)
+CREATE TABLE usuarios_datos (
+    id               BIGINT        PRIMARY KEY REFERENCES usuarios (id)
+                                   ON DELETE CASCADE
+  , nombre           VARCHAR(255)
+  , nick             VARCHAR(255)  NOT NULL UNIQUE
+  , localidad        VARCHAR(255)
+  , provincia        VARCHAR(255)
+  , direccion        VARCHAR(255)
+  , telefono         VARCHAR(255)
+  , biografia        VARCHAR(255)
   , fecha_nacimiento DATE
-  , geoloc           varchar(255)
+  , geoloc           VARCHAR(255)
   , genero           CHAR       -- RESTRINGIR A M(Másculino) F(Femenino)
 );
 
+CREATE INDEX idx_usuarios_datos_nick ON usuarios_datos (nick);
+
 
 ---------------------------------------------------
--- Licencias --
+--                  Licencias                    --
 ---------------------------------------------------
-
 DROP TABLE IF EXISTS licencias CASCADE;
-
-CREATE TABLE licencias
-(
-    id         BIGSERIAL    PRIMARY KEY
-  , tipo       VARCHAR(255) NOT NULL UNIQUE
-  , URL        VARCHAR(255) NOT NULL UNIQUE
+CREATE TABLE licencias (
+    id               BIGSERIAL     PRIMARY KEY
+  , tipo             VARCHAR(255)  NOT NULL UNIQUE
+  , URL              VARCHAR(255)  NOT NULL UNIQUE
 );
 
-
-
+CREATE INDEX idx_licencias_tipo ON licencias (tipo);
 
 ---------------------------------------------------
--- TORRENTS --
+--                   TORRENTS                    --
 ---------------------------------------------------
-
-
 DROP TABLE IF EXISTS torrents CASCADE;
-
-CREATE TABLE torrents
-(
-    id          BIGSERIAL    PRIMARY KEY
-  , licencia_id BIGINT       NOT NULL REFERENCES licencias (id)
-  , descripcion TEXT(500)
+CREATE TABLE torrents (
+    id              BIGSERIAL     PRIMARY KEY
+  , licencia_id     BIGINT        NOT NULL REFERENCES licencias (id)
+  , titulo          VARCHAR(255)  NOT NULL
+  , resumen         VARCHAR(255)  NOT NULL
+  , descripcion     TEXT(500)
+  , imagen          VARCHAR(255)
+  , file            VARCHAR(255)  -- Archivo .torrent
+  , magnet          VARCHAR(255)  -- enlace magnet al torrent
 );
 
-
-
-
-
-
 ---------------------------------------------------
--- COMENTARIOS --
+--                 COMENTARIOS                   --
 ---------------------------------------------------
 DROP TABLE IF EXISTS comentarios CASCADE;
 
@@ -105,25 +93,25 @@ DROP TABLE IF EXISTS comentarios CASCADE;
  * Un comentario puede ser hijo de otro (ser una respuesta a otro comentario)
  */
 CREATE TABLE comentarios (
-    id                bigserial       PRIMARY KEY
-  , usuario_id        bigint          NOT NULL REFERENCES "usuarios" (id)
-                                          ON DELETE NO ACTION
-                                          ON UPDATE CASCADE
-  , torrent_id    bigint          NOT NULL REFERENCES torrents (id)
-ON DELETE CASCADE ON UPDATE CASCADE
-  , contenido         TEXT(500)    NOT NULL
-  , comentario_id     bigint          REFERENCES comentarios (id)
-ON DELETE NO ACTION ON UPDATE CASCADE
-  , created_at        timestamp(0)    NOT NULL DEFAULT localtimestamp
-  , updated_at        timestamp(0)
-  , deleted           boolean         DEFAULT FALSE
+    id              BIGSERIAL  PRIMARY KEY
+  , usuario_id      BIGINT     NOT NULL REFERENCES "usuarios" (id)
+                               ON DELETE NO ACTION
+                               ON UPDATE CASCADE
+  , torrent_id      BIGINT     NOT NULL REFERENCES torrents (id)
+                               ON DELETE CASCADE
+                               ON UPDATE CASCADE
+  , contenido       TEXT(500)  NOT NULL
+  , comentario_id   BIGINT     REFERENCES comentarios (id)
+                               ON DELETE NO ACTION
+                               ON UPDATE CASCADE
+  , created_at      TIMESTAMP(0)  NOT NULL DEFAULT LOCALTIMESTAMP
+  , updated_at      TIMESTAMP(0)
+  , deleted         BOOLEAN    DEFAULT FALSE
 );
 
 CREATE INDEX idx_comentarios_usuario_id ON comentarios (usuario_id);
 CREATE INDEX idx_comentarios_torrent_id ON comentarios (torrent_id);
 CREATE INDEX idx_comentarios_comentario_id ON comentarios (comentario_id);
-
-
 
 
 ---------------------------------------------------
@@ -135,9 +123,8 @@ DROP TABLE IF EXISTS usuarios_bloqueados CASCADE;
  * Usuarios que han sido bloqueados
  */
 CREATE TABLE usuarios_bloqueados (
-    id            bigserial    PRIMARY KEY
-  , usuario_id    bigint       NOT NULL REFERENCES "usuarios" (id)
-  , created_at    TIMESTAMP(0) DEFAULT  localtimestamp
+    id            BIGSERIAL    PRIMARY KEY
+  , usuario_id    BIGINT       NOT NULL REFERENCES "usuarios" (id)
+  , created_at    TIMESTAMP(0) DEFAULT  LOCALTIMESTAMP
 );
-
 
