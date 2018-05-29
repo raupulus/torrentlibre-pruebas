93839464a65de92398a3b7d6732e6197f42ae5ec
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-05-29 16:56:53 +0200

    Añado más datos de prueba y campo tamaño para torrents. También agrego cabecera con autoría y licencia

--- db/torrentlibre.sql
+++ db/torrentlibre.sql
@@ -1,6 +1,41 @@
----------------------------------------------------
---           Archivo de base de datos            --
----------------------------------------------------
+-- -*- encoding: utf-8 -*-
+-------------------------------------------------------------------------------
+--
+-- Author:       Raúl Caro Pastorino
+-- Email Author: tecnico@fryntiz.es
+-- Web Author:   http://www.fryntiz.es
+-- github:       https://github.com/fryntiz
+-- gitlab:       https://gitlab.com/fryntiz
+-- twitter:      https://twitter.com/fryntiz
+--
+-- Create Date: 2018
+-- Project Name: TorrentLibre
+-- Description: Sitio web para compartir torrents con licencias libres
+--
+-- Dependencies: Extensión "pgcrypto" para postgresql
+--
+-- Revision 0.01 - File Created
+-- Additional Comments: Archivo principal con estructura de tablas
+--
+-- Código fuente: https://github.com/fryntiz/torrentlibre
+-------------------------------------------------------------------------------
+--
+-- Copyright (C) 2018  Raúl Caro Pastorino
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>
+-------------------------------------------------------------------------------
+
 
 ---------------------------------------------------
 --                     Roles                     --
@@ -108,6 +143,7 @@ CREATE TABLE usuarios (
                                    ON DELETE CASCADE
                                    ON UPDATE CASCADE
   , avatar           VARCHAR(255)
+  , lastlogin_at     TIMESTAMP     -- Última fecha de login
 );
 
 
